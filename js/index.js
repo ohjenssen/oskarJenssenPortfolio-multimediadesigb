755ab84ad832391549285698258b897b75c10499
ohjenssen <oskar_jenssen@hotmail.com>
2024-09-04 20:57:46 +0200

    more style changes

--- js/index.js
+++ js/index.js
@@ -8,13 +8,13 @@ function seeMore(e){
 }
 
 function header(){
-    window.addEventListener('scroll', function (e) {
-        if(window.pageYOffset > 75){
-            document.querySelector('.nav-container').classList.add('is-scrolling');
-        } else {
-            document.querySelector('.navigation-link').classList.remove('is-scrolling');
-        }
-    });
+    // window.addEventListener('scroll', function (e) {
+    //     if(window.pageYOffset > 75){
+    //         document.querySelector('.nav-container').classList.add('is-scrolling');
+    //     } else {
+    //         document.querySelector('.navigation-link').classList.remove('is-scrolling');
+    //     }
+    // });
 
     const menu_btn = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.mobile-nav');
@@ -30,7 +30,7 @@ switch(path){
     case '/index.html':
     case '/portfolio/':
         case '/portfolio/index.html':
-        document.querySelector('#seeMore').addEventListener('click', seeMore);
+        // document.querySelector('#seeMore').addEventListener('click', seeMore);
         break;
 }
 
